516cfb5ba0b90d7768fe02a843a3d716d783958d
MartinCesano <cesano305@gmail.com>
2024-06-19 12:27:34 -0300

    Formato de visualizacion de objetos en el archivo src/boliche.migrations/admin.py y agregue el readme

--- src/boliche/admin.py
+++ src/boliche/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from boliche.models import Ingrediente, Barra, Consumicion, Venta, DetalleVenta, DetalleConsumicion
 
-admin.site.register(Ingrediente)
-admin.site.register(Barra)
-admin.site.register(Consumicion)
-admin.site.register(Venta)
-admin.site.register(DetalleVenta)
-admin.site.register(DetalleConsumicion)
+@admin.register(Ingrediente)
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'marca')  # Campos a mostrar en la lista
+
+@admin.register(Barra)
+class BarraAdmin(admin.ModelAdmin):
+    list_display = ('numeroBarra', 'ubicacion')
+
+@admin.register(Consumicion)
+class ConsumicionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio')
+
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'fecha', 'hora', 'barra')
+
+@admin.register(DetalleVenta)
+class DetalleVentaAdmin(admin.ModelAdmin):
+    list_display = ('venta', 'consumicion', 'cantidad', 'precio')
